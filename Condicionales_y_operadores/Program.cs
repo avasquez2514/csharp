652683c6ad7937c6365b56dbbe90394e3b6c36e8
Anderson <anderzon.0077@gmail.com>
2024-09-14 11:44:13 -0500

    Añadir codigo operadores y condicionales

---
+++ Condicionales_y_operadores/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+CONDICIONALES
+string mytext = "Anderson"
+Console.WriteLine("Ingresa un numero: " );
+int num = int.Parse(console.ReadLine());
+if (num > 0)   Console.WriteLine(" positivo " );  
+else if (num < 0)  Console.WriteLine(" negativo " );  
+else Console.WriteLine(" Es cero " );  
+
+*/ 
+
+// OPERADORES LOGICOS
+
+Console.WriteLine("Ingresa el primer numero: " );
+double a double.Parse(console.ReadLine());
+Console.WriteLine("Ingresa el segundo numero: " );
+double b double.Parse(console.ReadLine());
+const string Indeterminado = "Indeterminado"
+Console.WriteLine($"Suma: {a+b}, Resta: {a-b}, Multiplicacion: {a*b} Division: {b!=0 ? a/b: Indeterminado}" );
